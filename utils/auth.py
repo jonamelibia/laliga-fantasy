82ecb150810dcae881912a4686bae1ac7c13c097
Jon Amelibia <jon.amelibia@alumni.mondragon.edu>
2025-08-14 13:24:15 +0200

    flow simplified

--- utils/auth.py
+++ utils/auth.py
@@ -18,18 +18,12 @@ def login_required():
             if username == USER and password == PASS:
                 st.session_state.logged_in = True
                 st.session_state.user = username
-                st.session_state.show_continue = True
+                st.success(f"✅ Bienvenido, {username}, vuelva a pulsar el botón para continuar.")
             else:
                 st.error("❌ Usuario o contraseña incorrectos")
-                st.session_state.show_continue = False
-        st.stop()  # Bloquear ejecución hasta login exitoso
 
-    # Una vez logueado, mostrar botón "Continuar" si no se ha pulsado aún
-    if st.session_state.show_continue:
-        if st.button("Continuar"):
-            st.session_state.show_continue = False  # Oculta el botón al continuar
-        else:
-            st.stop()  # Bloquea hasta que se pulse "Continuar"
+        st.stop()  # Evita que se ejecute el resto del script hasta hacer login
+
 def logout_button():
     if st.sidebar.button("Cerrar sesión"):
         st.session_state.logged_in = False
